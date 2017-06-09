cf0dfb526d781f8dad93dbd8a6684631e3a1a10f
Roman Pyrih <NoNTvonG@gmail.com>
2017-06-09 12:15:49 +0200

    Enemy

---
+++ EnemyMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class EnemyMove : MonoBehaviour 
+{
+	public float Speed = 5f; // Швидкість обєкта
+	private Transform AllControlPosition; // Тримаємо всі контрольні точки 
+	private Transform Waypoint; // Дані про кожну окрему точку
+	private int WaypointIndex = -1; // Перша контрольна точа
+
+	void Start()
+	{
+		AllControlPosition = GameObject.Find("Waypoints").transform; // записуємо всі контрольні точки з обєкту (Waypoints)
+		NextWayPoint(); // функція ідповідає за маршрут ворогів
+	}
+	void Update()
+	{	
+		Vector3 dir = Waypoint.transform.position - transform.position; // записуємо відстань між обєктом і точкою
+		dir.y = 0; // Кажемо то що ні в верх ні в низ він не може міняти позицію
+		transform.Translate(dir.normalized * Time.deltaTime * Speed);
+		if(dir.magnitude <= Time.deltaTime * Speed)
+		{
+			NextWayPoint(); // функція відповідає за перехід на наступний вейпоінт
+		}
+	}
+	private void NextWayPoint()
+	{
+		WaypointIndex++; // перехід на наступний вейпоінт
+		if(WaypointIndex >= AllControlPosition.childCount) // childCount -> це нумерована кількість всіх точок
+		{
+			LivesDown(); // функція відповідає за зменшення ХП гравця 
+			return;
+		}
+		Waypoint = AllControlPosition.GetChild(WaypointIndex); // GetChild -> показує індекс обєкта
+	}
+	private void LivesDown()
+	{
+		PlayerStats.Lives--; // віднімаємо ХП ігрока
+		Destroy(gameObject); // знищуємо ворога
+	}
+}
---
+++ Spawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Spawner : MonoBehaviour 
+{
+	public UnityEngine.UI.Text _Wave; // на якому рівні знаходиться гравець
+	public UnityEngine.UI.Text _NextWave; // наступний рівень через
+	public UnityEngine.UI.Text _Money; // кількість грошей гравця
+	public Transform EnemyPrefab; // Створюємо прифаб
+	public float StaticTimeSpawn = 5f; // Інтервал виходу гоблінів
+	public float TimeSpawn = 10f; // Час першого виходу гоблінів
+	private int WaveNumber = 0; // стартовий рівенль
+
+	void Update()
+	{
+		_Wave.text = "Wave: " + WaveNumber; // відображаємо рівень гравця
+		_NextWave.text = "Next wave: " + TimeSpawn.ToString("F2"); // наступний рівень
+		_Money.text = "Money: " + PlayerStats.Money + "$"; // і кількість грошей
+
+		
+		if(TimeSpawn <= 0f) // перевіряємо таймер. Якщо він <= 0 то спавнимо ворогів
+		{
+			StartCoroutine(SpawnWave());
+			TimeSpawn = StaticTimeSpawn; // обновлюємо таймер
+		}
+		TimeSpawn -= Time.deltaTime; // Обновлення кадрів
+	}
+
+	IEnumerator SpawnWave() // функція відповідає за затримку виходу ворогів
+	{
+		WaveNumber++; // перехід на наступинй рівель
+		for (int i = 0; i < WaveNumber; i++) // цикл який регулує частоту виходу ворогів
+		{
+			SpawnEnemy(); // спавнимо ворогів
+			yield return new WaitForSeconds(0.5f); // ставимо інтервал виходу ворогів в 0.5с
+		}
+	}
+	void SpawnEnemy() // ставн ворога
+	{
+		Instantiate(EnemyPrefab, transform.position, transform.rotation); // створюємо обєкт противника
+	}
+}
