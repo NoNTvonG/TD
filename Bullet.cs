252b1228266a6eb238b282d211018826a92d399d
Roman Pyrih <NoNTvonG@gmail.com>
2017-06-11 15:03:49 +0200

    Bullet & Live

---
+++ Bullet.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+	public GameObject EffectBullet; // ефект розлому пулі
+	public GameObject EffectEnemy; // ефект вбивства ворога
+	private Transform target; // вибраний ворог
+	public float Speed = 70f; // Швидкість пулі
+
+	public void X (Transform _target)
+	{
+		target = _target;
+	}
+	void Update()
+	{
+		if(target == null) // Якщо пуля досягнула свого обєкту то знищуємо її
+		{
+			Destroy(gameObject); // знищуємо обєкт
+			return;
+		}
+
+		Vector3 dir = target.position - transform.position; // Вираховуємо відстань між обєктами
+		float Distance = Speed * Time.deltaTime; // Записуємо дистанцію 
+
+		if(dir.magnitude <= Distance) // magnitude -> показує довжину цього вектору і якщо вона менша дистанції то ...
+		{
+			HitTower(); // функція попадання в ворога
+			return;
+		}
+
+		transform.Translate(dir.normalized * Distance, Space.World); // normalized -> повертаємо цей вектор з величиною 1
+	}
+
+	void HitTower()
+	{
+		GameObject effects = (GameObject)Instantiate(EffectBullet, transform.position, transform.rotation); // відображаємо ефект пулі
+		GameObject effects2 = (GameObject)Instantiate(EffectEnemy, transform.position, transform.rotation); // відображаємо ефект вбивства ворога
+		Destroy(effects, 1f); // видаляємо ефект
+		Destroy(effects2, 1f); // видаляємо ефект
+		
+		PlayerStats.Money += 1; // за вбивство додажмо гроші 
+
+		Destroy(target.gameObject); // видаляємо ворога
+	}
+}
---
+++ GameLives.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameLives : MonoBehaviour 
+{
+	public UnityEngine.GameObject Panel; // панель Game Over
+	public Text _GameLives; // панель життя гравця
+
+	void Start()
+	{
+		Panel.SetActive(false); // деактивуємо панель
+		Time.timeScale = 1; // швидкість гри 100%
+	}
+	void Update()
+	{
+		_GameLives.text = "Lives: " + PlayerStats.Lives.ToString(); // Відображаємо життя гравця
+
+		if(PlayerStats.Lives <= 0) // перевірка якщо ХП <= 0 
+		{
+			Panel.SetActive(true); // відображаємо панель
+			Time.timeScale = 0; // Ставимог гру на паузу
+		}
+	}
+	public void BackGame() // кнопка яка відповідає за повернення на головне мення
+	{
+		SceneManager.LoadScene(0); // відкриваємо нульову сцену
+	}
+}
