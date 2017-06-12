60b97a6b0f18d2157e7701e6f9e818c7f467a09f
Roman Pyrih <NoNTvonG@gmail.com>
2017-06-12 23:04:24 +0200

    Menu

---
+++ GameMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour 
+{
+	public UnityEngine.GameObject _MenuPanel;
+	public UnityEngine.UI.Button _Menu;
+	public UnityEngine.UI.Button _Options;
+	public UnityEngine.UI.Button _Back;
+	public UnityEngine.UI.Button _Exit;
+	public static bool SM = false; // Забороняє будувати башні коли включена пауза
+	void Start () 
+	{
+		_MenuPanel.SetActive(false); // стандартно меню деактивоване
+	}
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			SM = true; // забороняємо будувати башні
+			_MenuPanel.SetActive(true); // відкриваємо веню
+			Time.timeScale = 0; // зупиняємо час
+		}
+	}
+	public void Menu()
+	{	
+		SM = false; // дозворяє будувати башні
+		SceneManager.LoadScene(0); // загружаємо головне меню
+	}
+	public void Back()
+	{
+		SM = false; // дозворяє будувати башні
+		_MenuPanel.SetActive(false); // деактивовує панель
+		Time.timeScale = 1; // відновлємо ігровий час
+	}
+	public void Exit()
+	{
+		Application.Quit(); // закриваємо гру
+		Debug.Log("ff");
+	}
+}
---
+++ StartMenu.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour 
+
+{
+    public GUISkin skin;
+	public int Window;
+	void Start () 
+	{
+		Window = 1;
+	}
+	
+	void OnGUI()
+	{
+        GUI.skin = skin;
+		GUI.BeginGroup(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 400, 400));
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        if (Window == 1)
+        {
+            if (GUI.Button(new Rect(10, 30, 180, 30), "Play"))
+            {
+                Window = 2;
+            }
+            if (GUI.Button(new Rect (10, 70, 180, 30), "Options"))
+            {
+                Window = 3;
+            }
+            if (GUI.Button(new Rect(10, 110, 180, 30), "Exit"))
+            {
+                Window = 4;
+            }
+        }
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        if (Window == 2)
+        {
+            if (GUI.Button(new Rect(10, 30, 180, 30), "New Game"))
+            {
+				Window = 5;
+            }
+            if (GUI.Button(new Rect(10, 70, 180, 30), "Continue"))
+            {
+                //SceneManager.LoadScene(1);
+            }
+            if (GUI.Button(new Rect(10, 110, 180, 30), "Back"))
+            {
+                Window = 1;
+            }
+        }
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        if (Window == 3)
+        {   
+            AudioListener.volume = GUI.HorizontalSlider(new Rect(10, 50, 180, 10), AudioListener.volume, 0.0F, 1.0F);
+            if (GUI.Button(new Rect(10, 80, 180, 30), "Back"))
+            {
+                Window = 1;
+            }
+        }
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        if (Window == 4)
+        {
+            GUI.Label(new Rect(85, 30, 180, 30), "Exit?");
+            if (GUI.Button(new Rect(10, 70, 180, 30), "Yes"))
+            {
+                Application.Quit();
+            }
+            if (GUI.Button(new Rect(10, 110, 180, 30), "No"))
+            {
+                Window = 1;
+            }
+        }
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+		if (Window == 5)
+		{
+			GUI.Box(new Rect(10, 30, 180, 25), "Game Level");
+			if(GUI.Button(new Rect(25, 70, 150, 25), "LVL 1"))
+			{
+				SceneManager.LoadScene(1);
+			}
+			if(GUI.Button(new Rect(25, 110, 150, 25), "LVL 2"))
+			{
+				SceneManager.LoadScene(2);
+			}
+			if(GUI.Button(new Rect(25, 150, 150, 25), "LVL 3"))
+			{
+				SceneManager.LoadScene(3);
+			}
+			if(GUI.Button(new Rect(25, 190, 150, 25), "Back"))
+			{
+				Window = 2;
+			}
+		}
+        GUI.EndGroup();
+	}
+}
